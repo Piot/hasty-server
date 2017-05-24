b9315149496ea9a15e42851a370e3e843fc63c07
Peter Bjorklund <piot@hotmail.com>
2017-05-24 13:48:58 +0200

    Added tests

--- ofilepath/ofilepath_test.go
+++ ofilepath/ofilepath_test.go
@@ -1,6 +1,7 @@
 package ofilepath
 
 import (
+	"log"
 	"testing"
 
 	"github.com/piot/hasty-protocol/opath"
@@ -15,4 +16,46 @@ func TestFileCreation(t *testing.T) {
 	if ofilePath.ToString() != "/games/12/12/0/users/13404" {
 		t.Fail()
 	}
+}
+
+func checkPath(t *testing.T, path string, valid bool) {
+	opathExample, err := opath.NewFromString(path)
+	if (err == nil) && !valid {
+		t.Errorf("'%s' is not a valid path and should fail", path)
+	} else if (err != nil) && valid {
+		t.Errorf("Failed '%s' should be a valid path", path)
+	}
+	ofpExample, ofpErr := NewFromOPath(opathExample)
+	if (ofpErr == nil) && !valid {
+		t.Errorf("'%s' is not a valid path and should fail", path)
+	} else if (ofpErr != nil) && valid {
+		t.Errorf("Failed '%s' should be a valid path", path)
+	}
+	if valid {
+		log.Printf("Checked '%s'", ofpExample)
+	} else {
+		log.Printf("Not passed '%s'", path)
+	}
+}
+
+func TestPath(t *testing.T) {
+	checkPath(t, "/zaphod", true)
+	checkPath(t, "/zaphod2", true)
+	checkPath(t, "/32/42numbersallowedatstart", true)
+	checkPath(t, "/_underscore_is_allowed", true)
+	checkPath(t, "/@zaphod", true)
+	checkPath(t, "/zaphod/@3", true)
+	checkPath(t, "/zaphod/@01", true)
+
+	checkPath(t, "/endslashnotallowed/", false)
+	checkPath(t, "/something/åaphod", false)
+	checkPath(t, "/23#@aa/23", false)
+	checkPath(t, "musthaveroot", false)
+	checkPath(t, "/root/endslashnotallowed/", false)
+	checkPath(t, "/endslashnotallowed_/", false)
+	checkPath(t, "*/", false)
+	checkPath(t, "", false)
+	checkPath(t, "asdf", false)
+	checkPath(t, "/z aphod", false)
+	checkPath(t, " /zaphod", false)
 }
